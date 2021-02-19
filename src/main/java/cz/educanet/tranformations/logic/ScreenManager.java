be6fda9f16ee6c202dd5751d31cc9bfb2c020b8b
Nikolay <nikolay.sintsin@educanet.cz>
2021-02-19 23:22:05 +0100

    LMAO

--- src/main/java/cz/educanet/tranformations/logic/ScreenManager.java
+++ src/main/java/cz/educanet/tranformations/logic/ScreenManager.java
@@ -25,7 +25,29 @@ public class ScreenManager {
         return selectedPoints;
     }
 
-    public boolean isFilledIn(Coordinate coordinate) { // TODO: Implement this
-        return false;
+    private int fun(Coordinate x, Coordinate y, Coordinate z) {
+        return (x.getX() - z.getX()) - (z.getX() - y.getX()) * (y.getY() - z.getY()) * (z.getY() - x.getY());
+    }
+
+    public boolean isFilledIn(Coordinate coordinate) {
+
+        Coordinate[] list = selectedPoints.toArray(new Coordinate[]{});
+
+        Coordinate x = list[0];
+        Coordinate y = list[1];
+        Coordinate z = list[2];
+
+        int thirdDimX = ((coordinate.getX() - z.getX()) * (y.getY() - z.getY())) + ((coordinate.getY()) - z.getY()) * (z.getX() - y.getX());
+        int thirdDimY = ((coordinate.getX() - z.getX()) * (z.getY() - x.getY())) + ((coordinate.getY()) - z.getY()) * (x.getX() - z.getX());
+        int thirdDimZ = fun(x, y, z) - thirdDimX - thirdDimY;
+
+        int min = Math.min(fun(x, y, z), 0);
+        int max = Math.max(fun(x, y, z), 0);
+
+        if ((thirdDimX < min || thirdDimX > max) || (thirdDimY < min || thirdDimY > max)) {
+            return true;
+        } else {
+            return thirdDimZ >= min && thirdDimZ <= max;
+        }
     }
 }
